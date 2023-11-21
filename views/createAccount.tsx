f78859f5bfc723ee27cd5a67ab0a74f6fc036251
GlennOnyango <glennonyango@yahoo.com>
2023-11-21 16:55:00 +0300

    sign up ui fix

--- views/createAccount.tsx
+++ views/createAccount.tsx
@@ -1,7 +1,7 @@
-import { StyleSheet, Text, View } from "react-native";
-import { Button, Card, Icon, TextInput, useTheme } from "react-native-paper";
-import { useEffect, useMemo, useState } from "react";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { KeyboardAvoidingView, Platform, StyleSheet, View } from "react-native";
+import { Button, Text, TextInput, useTheme } from "react-native-paper";
+import { useMemo, useState } from "react";
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import { usePost } from "../customHooks/reactQuery/usePost";
 import SelectDropdown from "react-native-select-dropdown";
 import { ScrollView } from "react-native-gesture-handler";
@@ -23,7 +23,6 @@ export default function CreateAccount({ navigation }: Props) {
   const theme = useTheme();
   const { data, error, reset, mutate, isLoading, isSuccess } =
     usePost("/auth/register");
-  const [genericError, setGenericError] = useState<string>("");
   const [credentials, setCredentials] = useState<userCredntial>({
     phoneNumber: "",
     password: "",
@@ -41,7 +40,7 @@ export default function CreateAccount({ navigation }: Props) {
 
   const dis = useMemo(() => {
     if (credentials.userType === "league admin") {
-      return credentials.password.length > 8 &&
+      return credentials.password.length > 7 &&
         credentials.email.length > 9 &&
         credentials.phoneNumber.length > 9 &&
         credentials.teamID.length > 0 &&
@@ -51,7 +50,7 @@ export default function CreateAccount({ navigation }: Props) {
     }
 
     if (credentials.userType === "league participant") {
-      return credentials.password.length > 8 &&
+      return credentials.password.length > 7 &&
         credentials.email.length > 9 &&
         credentials.phoneNumber.length > 9 &&
         credentials.teamID.length > 0
@@ -66,205 +65,194 @@ export default function CreateAccount({ navigation }: Props) {
     navigation.navigate("Login");
   }
 
-  // useEffect(() => {
-
-  //   if (error) {
-  //     reset();
-  //   }
-
-  //   if (data) {
-  //     reset();
-  //   }
-
-  // }, [data, error, isLoading, isSuccess]);
-
   return (
     <ScrollView>
-      <View style={style.container}>
-        <View style={{ alignItems: "center", justifyContent: "center" }}>
-          <MaterialCommunityIcons
-            name="security-network"
-            size={102}
-            color={theme.colors.secondary}
-          />
-        </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <View style={style.container}>
+          <View style={{ alignItems: "center", justifyContent: "center" }}>
+            <MaterialIcons
+              name="app-registration"
+              size={102}
+              color={"grey"}
+            />
 
-        <View style={style.containerGroup}>
-          <SelectDropdown
-            data={["Select user type", "league participant", "league admin"]}
-            onSelect={(selectedItem, index) => {
-              updateCredentials({
-                type: "userType",
-                text: selectedItem,
-              });
-            }}
-            buttonTextAfterSelection={(selectedItem, index) => {
-              return selectedItem;
-            }}
-            rowTextForSelection={(item, index) => {
-              return item;
-            }}
-            renderDropdownIcon={() => (
-              <MaterialCommunityIcons
-                name="menu-down"
-                size={20}
-                color={"grey"}
-              />
-            )}
-            buttonStyle={{
-              width: "100%",
-              height: 50,
-              backgroundColor: "#fff",
-              borderBlockColor: "grey",
-              borderRadius: 5,
-              borderWidth: 1,
-            }}
-            buttonTextStyle={{
-              color: "#000",
-              textAlign: "left",
-            }}
-            dropdownStyle={{
-              width: "94%",
-              backgroundColor: theme.colors.background,
-            }}
-            rowStyle={{
-              backgroundColor: theme.colors.background,
-            }}
-            defaultValueByIndex={0}
-          />
-        </View>
-
-        <View style={style.containerGroup}>
-          <TextInput
-            style={style.input}
-            label="email"
-            mode="outlined"
-            onChangeText={(newText) =>
-              updateCredentials({ type: "email", text: newText })
-            }
-            value={credentials.email}
-            inputMode={"email"}
-          />
-        </View>
-
-        <View style={style.containerGroup}>
-          <TextInput
-            style={style.input}
-            label="Phone number"
-            mode="outlined"
-            onChangeText={(newText) =>
-              updateCredentials({ type: "phoneNumber", text: newText })
-            }
-            value={credentials.phoneNumber}
-            inputMode={"tel"}
-            keyboardType={"phone-pad"}
-            maxLength={12}
-          />
-        </View>
-        <View style={style.containerGroup}>
-          <TextInput
-            style={style.input}
-            label="Team id"
-            mode="outlined"
-            onChangeText={(newText) =>
-              updateCredentials({ type: "teamID", text: newText })
-            }
-            value={credentials.teamID}
-            inputMode={"text"}
-          />
-        </View>
-
-        {
-          // if user type is league admin
-          credentials.userType === "league admin" ? (
-            <View style={style.containerGroup}>
-              <TextInput
-                style={style.input}
-                label="League id"
-                mode="outlined"
-                onChangeText={(newText) =>
-                  updateCredentials({ type: "leagueID", text: newText })
-                }
-                value={credentials.leagueID}
-                inputMode={"text"}
-              />
-            </View>
-          ) : null
-        }
-
-        <View style={style.containerGroup}>
-          <TextInput
-            style={style.input}
-            label="Password"
-            mode="outlined"
-            onChangeText={(newText) =>
-              updateCredentials({ type: "password", text: newText })
-            }
-            value={credentials.password}
-            textContentType={"password"}
-            secureTextEntry={showPassword}
-            right={
-              <TextInput.Icon
-                icon="eye"
-                onPress={(e) => setShowPassword(!showPassword)}
-                forceTextInputFocus={false}
-              />
-            }
-          />
-        </View>
-
-        <View style={style.containerGroup}>
-          {genericError.length > 0 ? (
             <Text
               style={{
-                color: "red",
-                textAlign: "center",
-                marginVertical: 5,
+                color: theme.colors.secondary,
+                marginVertical: 16,
               }}
+              variant="titleLarge"
             >
-              {genericError}
-            </Text>
-          ) : null}
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            <Button
-              mode="outlined"
-              buttonColor={dis ? "#cccccc" : theme.colors.secondary}
-              textColor="#fff"
-              style={{
-                height: 50,
-                alignContent: "center",
-                justifyContent: "center",
-                borderColor: dis ? "#cccccc" : theme.colors.secondary,
-              }}
-              onPress={() => {
-                if (!dis) {
-                  setGenericError("Fill all fields to register");
-                  return;
-                } else {
-                  mutate(credentials);
-                }
-              }}
-            >
-              Register
-            </Button>
-
-            <Text style={{ textAlign: "center", marginVertical: 16 }}>
-              I already have an account.{" "}
-              <Text
-                style={{ color: theme.colors.secondary }}
-                onPress={() => navigation.navigate("Login")}
-              >
-                Access my account.
-              </Text>
+              Create Account
             </Text>
           </View>
+
+          <View style={style.containerGroup}>
+            <SelectDropdown
+              data={["Select user type", "league participant", "league admin"]}
+              onSelect={(selectedItem, index) => {
+                updateCredentials({
+                  type: "userType",
+                  text: selectedItem,
+                });
+              }}
+              buttonTextAfterSelection={(selectedItem, index) => {
+                return selectedItem;
+              }}
+              rowTextForSelection={(item, index) => {
+                return item;
+              }}
+              renderDropdownIcon={() => (
+                <MaterialCommunityIcons
+                  name="menu-down"
+                  size={20}
+                  color={"grey"}
+                />
+              )}
+              buttonStyle={{
+                width: "100%",
+                height: 50,
+                backgroundColor: "#fff",
+                borderBlockColor: "grey",
+                borderRadius: 5,
+                borderWidth: 1,
+              }}
+              buttonTextStyle={{
+                color: "#000",
+                textAlign: "left",
+              }}
+              dropdownStyle={{
+                width: "94%",
+                backgroundColor: theme.colors.background,
+              }}
+              rowStyle={{
+                backgroundColor: theme.colors.background,
+              }}
+              defaultValueByIndex={0}
+            />
+          </View>
+
+          <View style={style.containerGroup}>
+            <TextInput
+              style={style.input}
+              label="email"
+              mode="outlined"
+              onChangeText={(newText) =>
+                updateCredentials({ type: "email", text: newText })
+              }
+              value={credentials.email}
+              inputMode={"email"}
+            />
+          </View>
+
+          <View style={style.containerGroup}>
+            <TextInput
+              style={style.input}
+              label="Phone number"
+              mode="outlined"
+              onChangeText={(newText) =>
+                updateCredentials({ type: "phoneNumber", text: newText })
+              }
+              value={credentials.phoneNumber}
+              inputMode={"tel"}
+              keyboardType={"phone-pad"}
+              maxLength={12}
+            />
+          </View>
+          <View style={style.containerGroup}>
+            <TextInput
+              style={style.input}
+              label="Team id"
+              mode="outlined"
+              onChangeText={(newText) =>
+                updateCredentials({ type: "teamID", text: newText })
+              }
+              value={credentials.teamID}
+              inputMode={"text"}
+            />
+          </View>
+
+          {
+            // if user type is league admin
+            credentials.userType === "league admin" ? (
+              <View style={style.containerGroup}>
+                <TextInput
+                  style={style.input}
+                  label="League id"
+                  mode="outlined"
+                  onChangeText={(newText) =>
+                    updateCredentials({ type: "leagueID", text: newText })
+                  }
+                  value={credentials.leagueID}
+                  inputMode={"text"}
+                />
+              </View>
+            ) : null
+          }
+
+          <View style={style.containerGroup}>
+            <TextInput
+              style={style.input}
+              label="Password (min 8 characters)"
+              mode="outlined"
+              onChangeText={(newText) =>
+                updateCredentials({ type: "password", text: newText })
+              }
+              value={credentials.password}
+              textContentType={"password"}
+              secureTextEntry={showPassword}
+              right={
+                <TextInput.Icon
+                  icon="eye"
+                  onPress={(e) => setShowPassword(!showPassword)}
+                  forceTextInputFocus={false}
+                />
+              }
+            />
+          </View>
+
+          <View style={style.containerGroup}>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <Button
+                mode="outlined"
+                buttonColor={dis ? "#cccccc" : theme.colors.secondary}
+                textColor="#fff"
+                style={{
+                  height: 50,
+                  alignContent: "center",
+                  justifyContent: "center",
+                  borderColor: dis ? "#cccccc" : theme.colors.secondary,
+                }}
+                onPress={() => {
+                  if (!dis) {
+                    mutate(credentials);
+                  }
+                }}
+              >
+                Register
+              </Button>
+
+              <Text style={{ textAlign: "center", marginVertical: 16 }}>
+                I already have an account.{"  "}
+                <Text
+                  style={{ color: theme.colors.secondary }}
+                  onPress={() => navigation.navigate("Login")}
+                  variant="titleLarge"
+                >
+                  Login
+                </Text>
+              </Text>
+            </View>
+          </View>
         </View>
-      </View>
+      </KeyboardAvoidingView>
     </ScrollView>
   );
 }
@@ -273,6 +261,7 @@ const style = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+    justifyContent: "center",
     paddingHorizontal: 10,
     paddingTop: 20,
   },
